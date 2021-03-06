2dc397bf93f6154494296c94090048537ab3573b
upsgr <59116776+upsgr@users.noreply.github.com>
2021-03-06 22:58:29 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {Nav} from "./components/nav";
 import {SetupCards} from "./components/setup_cards";
 import {DeliveryCalc} from "./components/delivery_calc";
+import AdditionalOptions from "./components/additionalOptions"
 
 function App() {
   return (
     <div className="App">
       {/*<Nav/>*/}
-        <DeliveryCalc/>
+        <AdditionalOptions/>
     </div>
   );
 }
